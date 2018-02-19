c69f27685417d707a42df2bf355c57563b9e98b9
Denis Sorokin <denys.sorokin@gmail.com>
2018-02-19 12:46:09 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { AppRegistry } from 'react-native'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import AppReducer from './src/reducers'
+import AppWithNavigationState from './src/navigators/AppNavigator'
+import { middleware } from './src/utils/redux'
+
+const store = createStore(
+  AppReducer,
+  applyMiddleware(middleware),
+)
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <AppWithNavigationState />
+      </Provider>
+    )
+  }
+}
