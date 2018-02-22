ce6eff8e10a8c34bf0918a5075f5ee1afd572c62
Denis Sorokin <denys.sorokin@gmail.com>
2018-02-22 20:16:57 +0200

    Squashed commit of the following:
    commit 4969439982f8f7383fbe43c95f8dbca94a266907
    Author: Denis Sorokin <denys.sorokin@gmail.com>
    Date:   Thu Feb 22 20:14:09 2018 +0200
    
        Notifications works
    
    commit 362e60debd5a59e19e0a22d0dbf1f3ae2e9d6359
    Author: Denis Sorokin <denys.sorokin@gmail.com>
    Date:   Thu Feb 22 18:41:52 2018 +0200
    
        Added allow notifications
    
    commit 7a6d67f0142a967ed2165f34c42af2ded9e2d439
    Author: Denis Sorokin <denys.sorokin@gmail.com>
    Date:   Wed Feb 21 23:32:11 2018 +0200
    
        Added auth
    
    commit b3d03f053a9a2ba79deb1c2b5295048b389c5c80
    Author: Denis Sorokin <denys.sorokin@gmail.com>
    Date:   Wed Feb 21 21:08:18 2018 +0200
    
        Added firebase and nativebase

--- App.js
+++ App.js
@@ -2,7 +2,11 @@ import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { AppRegistry } from 'react-native'
 import { Provider } from 'react-redux'
+import { Permissions, Notifications } from 'expo'
 import { createStore, applyMiddleware } from 'redux'
+import * as firebase from 'firebase'
+import { firebaseApp } from './src/firebase'
+
 import AppReducer from './src/reducers'
 import AppWithNavigationState from './src/navigators/AppNavigator'
 import { middleware } from './src/utils/redux'
@@ -13,6 +17,54 @@ const store = createStore(
 )
 
 export default class App extends React.Component {
+
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      'Roboto': require('native-base/Fonts/Roboto.ttf'),
+      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+    })
+  }
+
+  componentDidMount = () => {
+    firebaseApp.auth().signInWithEmailAndPassword('b@b.com', 'bbbbbb')
+      .then(user => {
+        this.registerForPushNotificationsAsync(user)
+      })
+  }
+  registerForPushNotificationsAsync = async (user) => {
+    const { status: existingStatus } = await Permissions.getAsync(
+      Permissions.NOTIFICATIONS
+    );
+    let finalStatus = existingStatus;
+
+    // only ask if permissions have not already been determined, because
+    // iOS won't necessarily prompt the user a second time.
+    if (existingStatus !== 'granted') {
+      // Android remote notification permissions are granted during the app
+      // install, so this will only ask on iOS
+      const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+      finalStatus = status;
+    }
+
+    // Stop here if the user did not grant permissions
+    if (finalStatus !== 'granted') {
+      return;
+    }
+
+    // Get the token that uniquely identifies this device
+    let token = await Notifications.getExpoPushTokenAsync();
+
+     // POST the token to our backend so we can use it to send pushes from there
+     var updates = {}
+     updates['/expoToken'] = token
+     console.log(`--user: ${JSON.stringify(user)}`)
+     firebaseApp.database().ref('usersac').child(user.uid).update(updates)
+    //  firebase.database().ref('userac' + user.uid).set({
+    //   expoToken : token
+    // });
+     //call the push notification 
+  }
+
   render() {
     return (
       <Provider store={store}>
--- src/containers/LoginScreen.js
+++ src/containers/LoginScreen.js
@@ -1,42 +1,131 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { Button, StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
+import { Container, Content, Header, Form, Input, Item, Button, Label } from 'native-base'
+import * as firebase from 'firebase'
+import { firebaseApp } from '../firebase'
+
+class LoginScreen extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+    componentDidMount() {
+        firebaseApp.auth().onAuthStateChanged(user => {
+            if (user != null) {
+                console.log(user)
+            }
+        })
+    }
+
+    signUpUser = (email, password) => {
+        try {
+            if (this.state.password.length < 6) {
+                alert('Please enter atleast 6 characters')
+                return
+            }
+            firebaseApp.auth().createUserWithEmailAndPassword(email, password)
+        } catch (error) {
+            console.log(error.toString())
+        }
+    }
+
+    loginUser = (email, password) => {
+        try {
+            firebaseApp.auth().signInWithEmailAndPassword(email, password)
+                .then(user => {
+                    console.log(user)
+                })
+        } catch (error) {
+            console.log(error.toString())
+        }
+    }
+
+    async loginWithFacebook() {
+        const { type, token } = await Expo.Facebook.logInWithReadPermissionsAsync('174623653159521', { permissions: ['public_profile'] })
+        if (type == 'success') {
+            const credential = firebase.auth.FacebookAuthProvider.credential(token)
+            firebaseApp.auth().signInWithCredential(credential)
+                .catch(error => console.log(error))
+        }
+    }
+
+    render() {
+        return (
+            <Container style={styles.container}>
+                <Form>
+                    <Item floatingLabel>
+                        <Label>Email</Label>
+                        <Input
+                            autoCorrect={false}
+                            autoCapitalize='none'
+                            onChangeText={email => this.setState({ email })}
+                        />
+                    </Item>
+                    <Item floatingLabel>
+                        <Label>Password</Label>
+                        <Input
+                            secureTextEntry={true}
+                            autoCorrect={false}
+                            autoCapitalize='none'
+                            onChangeText={password => this.setState({ password })}
+                        />
+                    </Item>
+                    <Button
+                        style={{ marginTop: 10 }}
+                        full
+                        rounded
+                        success
+                        onPress={() => this.loginUser(this.state.email, this.state.password)}
+                    >
+                        <Text style={{ color: 'white' }}>Login</Text>
+                    </Button>
+                    <Button
+                        style={{ marginTop: 10 }}
+                        full
+                        rounded
+                        primary
+                        onPress={() => this.signUpUser(this.state.email, this.state.password)}
+                    >
+                        <Text style={{ color: 'white' }}>Sign Up</Text>
+                    </Button>
+                    <Button
+                        style={{ marginTop: 10 }}
+                        full
+                        rounded
+                        primary
+                        onPress={() => this.loginWithFacebook()}
+                    >
+                        <Text style={{ color: 'white' }}>Login with Facebook</Text>
+                    </Button>
+                </Form>
+
+            </Container>
+        )
+    }
+}
+
+
+LoginScreen.navigationOptions = {
+    title: 'Log In',
+}
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center',
         backgroundColor: '#F5FCFF',
+        padding: 10
     },
     welcome: {
         fontSize: 20,
         textAlign: 'center',
         margin: 10,
     },
-});
-
-const LoginScreen = ({ navigation }) => (
-    <View style={styles.container}>
-        <Text style={styles.welcome}>
-            Screen A
-    </Text>
-        <Text style={styles.instructions}>
-            This is great
-    </Text>
-        <Button
-            onPress={() => navigation.dispatch({ type: 'Login' })}
-            title="Log in"
-        />
-    </View>
-);
-
-LoginScreen.propTypes = {
-    navigation: PropTypes.object.isRequired,
-};
-
-LoginScreen.navigationOptions = {
-    title: 'Log In',
-};
+})
 
 export default LoginScreen
